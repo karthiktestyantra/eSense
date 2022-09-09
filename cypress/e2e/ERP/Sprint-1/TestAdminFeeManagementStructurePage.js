e4e90e1461c7f1d09f895cbb4ef00e1cfb5366f6
karthiktesyantra <karthik.k@testyantra.com>
2022-09-09 13:06:36 +0530

    commit

--- cypress/e2e/ERP/Sprint-1/TestAdminFeeManagementStructurePage.js
+++ cypress/e2e/ERP/Sprint-1/TestAdminFeeManagementStructurePage.js
@@ -6,12 +6,12 @@ describe("Verify Fee Management Fee Structure functionalities", function () {
 
   before(function () {
     cy.visit(Cypress.env("urlStagingERP"))
-  })
-
-  beforeEach(function () {
     cy.fixture("ERP/LoginCredentials").then(function (validLoginData) {
       loginPage.login(validLoginData.adminUsername, validLoginData.adminPassword)
     })
+  })
+
+  beforeEach(function () {
     cy.fixture("ERP/FeeManagement").as("feeManagement")
   })
 
@@ -33,5 +33,15 @@ describe("Verify Fee Management Fee Structure functionalities", function () {
 
   it("Validate the user is able to see the option to setup new fee master or Add new as the user logs in and is in the fee management mini-onboarding process flow/EE-79/ERP_TC_010", { tags: '@smoke' }, function () {
     feeManagementFeeStructurePage.verifyAddNewFeeStructurePopUp(this.feeManagement.addNewBtnPageTitle1, this.feeManagement.addNewBtnPageTitle2)
+  })
+
+  it("Validate the user is displayed with Details tab/modal as they click on setup new fee master button/EE-79/ERP_TC_011", function () {
+    feeManagementFeeStructurePage.clickOnSetUpFeeMastersButton()
+    feeManagementFeeStructurePage.verifyAddNewFeeStructurePopUp(this.feeManagement.addNewBtnPageTitle1, this.feeManagement.addNewBtnPageTitle2)
+    feeManagementFeeStructurePage.verifyAddNewFeeStructureDetailsPage()
+  // })
+
+  // it("Validate user can navigate to different tabs showing lock icon only by clicking continue button./EE-57/ERP_TC_007", { tags: '@smoke' }, function () {
+    feeManagementFeeStructurePage.enterAllDetails("school","description")
   })
 })

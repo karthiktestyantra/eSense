6bc85b4082ddc5e687756dd8a1251efd03bd9457
karthiktestyantra <karthik.k@testyantra.com>
2022-09-21 12:17:30 +0530

    360 report change

--- cypress/support/pageObjects/LMS-2/Admin360ReportPage.js
+++ cypress/support/pageObjects/LMS-2/Admin360ReportPage.js
@@ -1,55 +1,61 @@
-class Admin360ReportPage{
+class Admin360ReportPage {
 
-    get360ReportLnk(){
-        return cy.get('div.popper-sub-student div').contains("Student 360 Report")
-     }
-     get360ReportContents(){
-        return cy.get('table[aria-labelledby="tableTitle"] thead tr th')
-     }
-     getGradeDrpDwn(){
+   get360ReportLnk() {
+      return cy.get('div.popper-sub-student div').contains("Student 360 Report")
+   }
+   get360ReportContents() {
+      return cy.get('table[aria-labelledby="tableTitle"] thead tr th')
+   }
+   getGradeDrpDwn() {
       return cy.get('div div#demo-simple-select-1')
-     }
-     getGradeDrpDwnLst(){
+   }
+   getGradeDrpDwnLst() {
       return cy.get('div.MuiPopover-paper ul li[role="option"]')
-     }
-     getSectionDrpDwnLst(){
+   }
+   getSectionDrpDwnLst() {
       return cy.get('div.MuiPopover-paper ul li[role="option"]')
-     }
-     getSectionDrpDwn(){
+   }
+   getSectionDrpDwn() {
       return cy.get('div div#demo-simple-select-2')
-     }
-     getFrstNameLst(){
+   }
+   getFrstNameLst() {
       return cy.get('td p.MuiTypography-body1')
-     }
-     getSearchStudentSearchBar(){
+   }
+   getSearchStudentSearchBar() {
       return cy.get('input.MuiInputBase-inputAdornedStart')
-     }
-     getCheckbxLst(){
+   }
+   getCheckbxLst() {
       return cy.get('input[type="checkbox"]')
-     }
-     getUploadCSVBtn(){
+   }
+   getUploadCSVBtn() {
       return cy.get('button[type="button"]').contains("Upload CSV")
-     }
-     getGenerateReportBtn(){
+   }
+   getGenerateReportBtn() {
       return cy.get('button[type="button"]').contains("Generate report")
-     }
-     getDwnldReportBtn(){
+   }
+   getDwnldReportBtn() {
       return cy.get('button[type="button"]').contains("Download report")
-     }
-     getAllStudentCheckBx(){
+   }
+   getAllStudentCheckBx() {
       return cy.get('input[aria-label="select all students"]')
-     }
-     getGenerateBtnInGenerateReportPopup(){
+   }
+   getGenerateBtnInGenerateReportPopup() {
       return cy.get('button[name="buttons"]')
-     }
-     getCancelReportBtnInGenerateReportPopup(){
+   }
+   getCancelReportBtnInGenerateReportPopup() {
       return cy.get('button.dlt_outline')
-     }
-     getReportGenerateSuccessPopupCloseIcon(){
+   }
+   getReportGenerateSuccessPopupCloseIcon() {
       return cy.get('svg.close-icon_report')
-     }
-     get360ReportPageTitle(){
+   }
+   get360ReportPageTitle() {
       return cy.get('p.MuiTypography-body1').contains("360˚ Reports")
-     }
+   }
+   getFlagImgInReportPage(){
+      return cy.get('div img[class="alertImgcls"]')
+   }
+   getDisableCheckBxForFlagImg(){
+      return cy.xpath("//div//img[@class='alertImgcls']/ancestor::tr[@class='MuiTableRow-root css-1gqug66']/descendant::span[contains(@class,'chk')]")
+   }
 }
 export default Admin360ReportPage;

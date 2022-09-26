9ec4e3940ea82bc7924ef3235a06bcf49026316a
karthiktesyantra <karthik.k@testyantra.com>
2022-09-26 10:30:04 +0530

    commit

--- cypress/e2e/LMS/Sprint-20/TeacherCalender.js
+++ cypress/e2e/LMS/Sprint-20/TeacherCalender.js
@@ -19,25 +19,32 @@ describe("Verify Teacher Calender functionalities", function () {
         teacherClassGradePage.validateClassDetailBtn()
         teacherClassGradePage.validateTimetableBtn()
     })
+
     it('To validate user is redirected to class details screen upon clicking on ""Start session"" button/EL-6982/ES6982-02',function () {
         teacherClassGradePage.validateClassDetailBtn()
     })
+
     it('To validate user is able to view class resources details after clicking on "Class resources tab"/EL-6983/ES6983-01.', function () {
         teacherClassGradePage.validateResourceTable()
     })
+
     it('To validate "count of total resources attached " is displayed after user clicks on Class resources tab/EL-6983/ES6983-02', function () {
         teacherClassGradePage.validateCountOfTotalResourcesAttached()
     })
+
     it('To validate "Download all" button is available for the user after user clicks on class resources tab/EL-6983/ES6983-03', function () {
         teacherClassGradePage.validateDownloadBtn()
     })
+
     it('To validate preview and Downalod options is displayed against each file/EL-6983/ES6983-04', function () {
         teacherClassGradePage.validatePreviewIcon()
         teacherClassGradePage.validateDownloadIcon()
     })
+
     it('To valiadte user is able to downlaod individual resources/EL-6983/ES6983-07', function () {
         teacherClassGradePage.valiadteDownlaodIndividualResources()
     })
+
     it('To validate user is able to view the file after clickig on preview icon/EL-6983/ES6983-05', function () {
         teacherClassGradePage.validatepreviewPage()
         cy.go('back')
@@ -46,35 +53,43 @@ describe("Verify Teacher Calender functionalities", function () {
         teacherCalenderPage.clickOnGrade()
         teacherCalenderPage.clickOnStartSessionBtn()
     })
+
     it('To validate user is able to view and take attendance on clciking Attendance tab/EL-6986/ES6986-01', function () {
         teacherClassGradePage.clickOnAttendenceTab()
         teacherClassGradePage.validatePresentRadioBtn()
         teacherClassGradePage.validateAbsentRadioBtn()
     })
+
     it('To valiadte"Attendance tab"user able to mark and store attendance of the class/EL-6986/ES6986-03', function () {
         teacherClassGradePage.validatePresentRadioBtn()
         teacherClassGradePage.validateAbsentRadioBtn()
         teacherClassGradePage.validateSaveBtn()
     })
+
     it('To valiadte Attendance tab consist of total count of students invited/added to the class/EL-6986/ES6986-02', function () {
         teacherClassGradePage.validateAttendenceTablebody()
     })
+
     it('To validate user is able to view homework details by click on""Homework tab/EL-6984/ES6984-01',function () {
         teacherClassGradePage.clickOnHomeworkTab()
         teacherClassGradePage.valiadteHomeworkPageTitle()
     })
+
     it('To validate Homework tab consist of total count of homework assingned for the class/EL-6984/ES6984-02',function () {
         teacherClassGradePage.validateCountOfHomework()
     })
+
     it('To validate preview and Downalod options is displayed against each file/EL-6984/ES6984-04',function () {
         teacherClassGradePage.validateHomeworkPreviewIcon()
         teacherClassGradePage.validateHomeworkDownloadIcon()
     })
+
     it('To validate Homework attachment list consist of Homework title,created date ,count of attachments/EL-6984/ES6984-05',function () {
         teacherClassGradePage.validateHomeworkTitle()
         teacherClassGradePage.validateHomeworkDate()
         teacherClassGradePage.validateHomeworkAttachement()
     })
+
     it('To validate user is able to view the"Homework" after clickig on preview icon/EL-6984/ES6984-06',function () {
         teacherClassGradePage.clickOnHomeworkPreviewIcon()
         teacherClassGradePage.validateHomeworkPreviewPage()
@@ -84,17 +99,21 @@ describe("Verify Teacher Calender functionalities", function () {
         teacherCalenderPage.clickOnGrade()
         teacherCalenderPage.clickOnStartSessionBtn()
     })
+
     it('To validate individual resources is downloaded after user clicks on download/EL-6984/ES6984-07',function () {
         teacherClassGradePage.clickOnHomeworkTab()
         teacherClassGradePage.getHomeworkDownloadIcon().click()
     })
+
     it('To validate Download all button is available in homeworktab and user is able to download the homework/EL-6984/ES6984-03',function () {
         teacherClassGradePage.getHomeworkDownloadAllBtn().click()
     })
+    
     it('To vlaidate that lesson plan pop-up is populated after user clicks on view lesson plan/EL-6982/ES6982-03', function () {
         teacherClassGradePage.clickOnViewLessonPlanPage()
         teacherClassGradePage.validateLessonPlanPage()
     })
+    
     it('To valiadte dropdown is availabe in view lesson plan popup if there are multiple lesson plans/EL-6982/ES6982-04', function () {
         teacherClassGradePage.valiadteLessonDropdown()
     })

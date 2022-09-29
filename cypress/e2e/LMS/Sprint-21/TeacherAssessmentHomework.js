b6c877e037d5b97bbda494a71af4feffa586c73f
karthiktestyantra <karthik.k@testyantra.com>
2022-09-29 13:29:31 +0530

    29-commit

--- cypress/e2e/LMS/Sprint-21/TeacherAssessmentHomework.js
+++ cypress/e2e/LMS/Sprint-21/TeacherAssessmentHomework.js
@@ -16,9 +16,9 @@ describe("Verify Homework card - Sprint 21(EL-6426)", function () {
 
     it("EL-6426/ES6426_1 Validate user is able to view the homework created", function () {
         home.getMyclassLnk().click({ force: true })
-        home.getSubLstTxtInMyClass().each(($e1,index,$list)=>{
+        home.getSubLstTxtInMyClass().each(($e1, index, $list) => {
             const txt = $e1.text()
-            if(txt === "Tamil"){
+            if (txt === "Tamil") {
                 home.getsubLstInMyClass().eq(index).click()
                 return false;
             }
@@ -27,24 +27,66 @@ describe("Verify Homework card - Sprint 21(EL-6426)", function () {
         cy.wait(1000)
         homework.getCreateNewHomeworkBtn().click()
         homework.getCreateHomeWorkPopupScreen().should('be.visible')
-        homework.getCreateHomeworkTitleInPopup().should('have.text',"Create Homework").should('be.visible')
+        homework.getCreateHomeworkTitleInPopup().should('have.text', "Create Homework").should('be.visible')
         homework.getHomeworkTitleTxtFldInPopup().type("Ponniyan Selvan")
         homework.getDescriptionTxtFldInPopup().type("Created by Kalki in 18th century")
         homework.getClassAddHomeworkPopupDueDate().click();
-    const dayjs = require("dayjs");
-    const todaysDate = dayjs().format("MMM DD, YYYY");
-    var dayMonthYear = todaysDate.split(" ");
-    let dates = parseInt(dayMonthYear[1]) + 1;
-    cy.log(dates);
-    cy.get(".MuiPickersDay-dayWithMargin:visible").contains(dates).click();
-    cy.wait(1000);
-    homework.getClassAddHomeworkPopupDueDate().click({ force: true });
+        const dayjs = require("dayjs");
+        const todaysDate = dayjs().format("MMM DD, YYYY");
+        var dayMonthYear = todaysDate.split(" ");
+        let dates = parseInt(dayMonthYear[1]) + 1;
+        cy.log(dates);
+        homework.getDayWithMargin().contains(dates).click();
+        cy.wait(1000);
+        homework.getClassAddHomeworkPopupDueDate().click({ force: true });
         homework.getTimePickerInPopup().click()
         homework.getPMTimeInTimePicker().click()
-        homework.getTimePickerInPopup().click({force:true})
+        homework.getTimePickerInPopup().click({ force: true })
         homework.getApproxTimeRequiredDrpDwnInPopup().click()
         homework.getApproxTimeDrpDwnLstInPopup().first().click()
         homework.getSubmitBtnInCreateHomeworkPopup().click()
         homework.getHomeworkCreatedSuccessPopup().should('be.visible')
     })
+
+    it("EL-6426/ES6426_2 Validate when user clicks on View icon, the details of the homework is displayed", function () {
+        homework.getHomeWorkLstInAssessmentPage().each(($e1, index, $list) => {
+            const txt = $e1.text()
+            if (txt.includes("Ponniyan Selvan")) {
+                homework.getEditHomeWorkBtnLstInAssessmentPage().eq(index).scrollIntoView().click({ force: true })
+                homework.getTitleInEditHomeworkPage().should('have.text', "Ponniyan Selvan")
+                return false;
+            }
+        })
+    })
+
+    it("EL-6426/ES6426_4 Validate user is able to edit the homework", function () {
+        homework.getEditBtnInHomeworkPopup().should('be.visible').click()
+        homework.getHomeworkTitleTxtFldInPopup().clear().type("Naane Varuven")
+        homework.getSubmitBtnInCreateHomeworkPopup().click()
+        homework.getHomeworkUpdatedSuccessPopup().should('be.visible')
+    })
+
+    it("EL-6426/ES6426_3 Validate when user clicks on the 3 dots at the top of the card Duplicate and Edit icons is displayed", function () {
+        homework.getHomeWorkLstInAssessmentPage().each(($e1, index, $list) => {
+            const txt = $e1.text()
+            if (txt.includes("Naane Varuven")) {
+                homework.getActionBtnLstForHomeworkInAssessmentPage().eq(index).scrollIntoView().click({ force: true })
+                homework.getActionsLstInActionbtn().should('contain', "Edit").and('contain.text', "Duplicate")
+                //homework.getSubmissionBtnInHomeworkPage().click({force:true})
+            }
+        })
+    })
+
+    it("EL-6426/ES6426_3 Validate user should be able to delete the homework created", function () {
+        homework.getHomeWorkLstInAssessmentPage().each(($e1, index, $list) => {
+            const txt = $e1.text()
+            if (txt.includes("Naane Varuven")) {
+                homework.getDeleteHomeWorkBtnLstInAssessmentPage().eq(index).scrollIntoView().click({ force: true })
+                homework.getDeleteReminderPopup().should('be.visible')
+                homework.getDeleteBtnForHomeWorkInDeletePopup().click()
+                homework.getHomeWorkDeletedSuccessPopup().should('be.visible')
+            }
+        })
+    })
+
 })
--- cypress/support/pageObjects/LMS-2/TeacherAssessmentPage.js
+++ cypress/support/pageObjects/LMS-2/TeacherAssessmentPage.js
@@ -1,73 +1,97 @@
 class TeacherAssessmentPage {
-    getMyClassSubName() {
-        return cy.get('div.card_roman_txt').contains('Grade 3 A')
-     }
-     getAssessmentTab() {
-        return cy.get('button.MuiButtonBase-root').contains('Assessments')
-     }
-     getCreateNewHomeworkBtn(){
-        return cy.get('button.prime_btn')
-     }
-     getCreateHomeWorkPopupScreen(){
-        return cy.get('.add_homework_class-content')
-     }
-     getCreateHomeworkTitleInPopup(){
-        return cy.get('h1.add_homework_class-tittle')
-     }
-     getHomeworkTitleTxtFldInPopup(){
-        return cy.get('.schAdminInputCtr')
-     }
-     getDescriptionTxtFldInPopup(){
-        return cy.get('textarea.MuiInputBase-input:visible')
-     }
-     getDueDateInPopup(){
-        return cy.get('input[placeholder="dd/mm/yyyy"]')
-     }
-     getTodaysDateInDatePicker(){
-        return cy.get('button.MuiPickersDay-today')
-     }
-     getTimePickerInPopup(){
-        return cy.get('input[placeholder="h:mm (a|p)m"]')
-     }
-     getPMTimeInTimePicker(){
-        return cy.get('button span.MuiTypography-caption:visible').contains("PM")
-     }
-     get11PMTimePicker(){
-        return cy.get('div[role="listbox"] span[aria-label="11 hours"]:visible')
-     }
-     get45MinsTimePicker(){
-        return cy.get('div[role="listbox"] span[aria-label="45 minutes"]:visible')
-     }
-     getApproxTimeRequiredDrpDwnInPopup(){
-        return cy.get('div.addHmeSltInp')
-     }
-     getApproxTimeDrpDwnLstInPopup(){
-        return cy.get('div ul li.MuiMenuItem-gutters')
-     }
-     getSubmitBtnInCreateHomeworkPopup(){
-        return cy.get('button[type="submit"]')
-     }
-     getHomeworkCreatedSuccessPopup(){
-        return cy.contains("Homework Created!")
-     }
-     getHomeWorkLstInAssessmentPage(){
-        return cy.get('h6.mb-0')
-     }
-     getDeleteHomeWorkBtnLstInAssessmentPage(){
-        return cy.xpath("//div[contains(@class,'justify-content-end')]//div[2]")
-     }
-     getDeleteReminderPopup(){
-        return cy.get('p.delete_reminder-title').contains("Do you want to delete this homework?")
-     }
-     getDeleteBtnForHomeWorkInDeletePopup(){
-        return cy.get('button[data-testid="delete"]')
-     }
-     getHomeWorkDeletedSuccessPopup(){
-        return cy.contains('Homework deleted!')
-     }
-     getClassAddHomeworkPopupDueDate() {
-        return cy.get('input[placeholder="dd/mm/yyyy"]');
-      }
-      
- }
- module.exports = new TeacherAssessmentPage() 
+   getMyClassSubName() {
+      return cy.get('div.card_roman_txt').contains('Grade 3 A')
+   }
+   getAssessmentTab() {
+      return cy.get('button.MuiButtonBase-root').contains('Assessments')
+   }
+   getCreateNewHomeworkBtn() {
+      return cy.get('button.prime_btn')
+   }
+   getCreateHomeWorkPopupScreen() {
+      return cy.get('.add_homework_class-content')
+   }
+   getCreateHomeworkTitleInPopup() {
+      return cy.get('h1.add_homework_class-tittle')
+   }
+   getHomeworkTitleTxtFldInPopup() {
+      return cy.get('.schAdminInputCtr')
+   }
+   getDescriptionTxtFldInPopup() {
+      return cy.get('textarea.MuiInputBase-input:visible')
+   }
+   getDueDateInPopup() {
+      return cy.get('input[placeholder="dd/mm/yyyy"]')
+   }
+   getTodaysDateInDatePicker() {
+      return cy.get('button.MuiPickersDay-today')
+   }
+   getTimePickerInPopup() {
+      return cy.get('input[placeholder="h:mm (a|p)m"]')
+   }
+   getPMTimeInTimePicker() {
+      return cy.get('button span.MuiTypography-caption:visible').contains("PM")
+   }
+   get11PMTimePicker() {
+      return cy.get('div[role="listbox"] span[aria-label="11 hours"]:visible')
+   }
+   get45MinsTimePicker() {
+      return cy.get('div[role="listbox"] span[aria-label="45 minutes"]:visible')
+   }
+   getApproxTimeRequiredDrpDwnInPopup() {
+      return cy.get('div.addHmeSltInp')
+   }
+   getApproxTimeDrpDwnLstInPopup() {
+      return cy.get('div ul li.MuiMenuItem-gutters')
+   }
+   getSubmitBtnInCreateHomeworkPopup() {
+      return cy.get('button[type="submit"]')
+   }
+   getHomeworkCreatedSuccessPopup() {
+      return cy.contains("Homework Created!")
+   }
+   getHomeworkUpdatedSuccessPopup(){
+      return cy.contains('Homework Updated!')
+   }
+   getHomeWorkLstInAssessmentPage() {
+      return cy.get('h6.mb-0')
+   }
+   getDeleteHomeWorkBtnLstInAssessmentPage() {
+      return cy.xpath("//div[contains(@class,'justify-content-end')]//div[2]/img")
+   }
+   getEditHomeWorkBtnLstInAssessmentPage() {
+      return cy.xpath("//div[contains(@class,'justify-content-end')]//div[1]/img")
+   }
+   getDeleteReminderPopup() {
+      return cy.get('p.delete_reminder-title').contains("Do you want to delete this homework?")
+   }
+   getDeleteBtnForHomeWorkInDeletePopup() {
+      return cy.get('button[data-testid="delete"]')
+   }
+   getHomeWorkDeletedSuccessPopup() {
+      return cy.contains('Homework deleted!')
+   }
+   getClassAddHomeworkPopupDueDate() {
+      return cy.get('input[placeholder="dd/mm/yyyy"]');
+   }
+   getDayWithMargin() {
+      return cy.get(".MuiPickersDay-dayWithMargin:visible")
+   }
+   getTitleInEditHomeworkPage(){
+      return cy.get('div.view_homework_headings p')
+   }
+   getEditBtnInHomeworkPopup(){
+      return cy.get('button.edit_btn')
+   }
+   getActionBtnLstForHomeworkInAssessmentPage(){
+      return cy.get('button svg[data-testid="MoreVertIcon"]')
+   }
+   getActionsLstInActionbtn(){
+      return cy.get("div.MuiPaper-elevation8 button[type='button']:visible")
+   }
+   getSubmissionBtnInHomeworkPage(){
+      return cy.get('div[role="tablist"] button').contains("Submissions")
+   }
+
+}
+module.exports = new TeacherAssessmentPage() 

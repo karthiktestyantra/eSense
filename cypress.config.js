51ca1c5a1331752d6f7c6464c1605dc79d3d445c
karthiktestyantra <karthik.k@testyantra.com>
2022-10-12 11:45:40 +0530

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -12,7 +12,8 @@ module.exports = defineConfig({
     urlMain: 'https://wwe.staging.topschool.co.in',
     urlQA: 'https://srilanka.staging.topschool.co.in',
     urlStagingERP: "https://liverpool.staging.topschool.co.in",
-    urlBhsSchool : 'https://bhs.staging.topschool.co.in'
+    urlBhsSchool : 'https://bhs.staging.topschool.co.in',
+    urlProd : 'https://prodautomation1.thetopschool.com/'
   },
   video: false,
   defaultCommandTimeout: 20000,
---
+++ cypress/e2e/LMS-Prod/TeacherMyClassesStudentGroups.js
@@ -0,0 +1,22 @@
+const teacherDashboardPage = require("../../../support/pageObjects/LMS-2/TeacherDashboardPage")
+
+describe("Verify Admin Student GradeBook functionalities - Sprint 20(EL-6975)", function () {
+
+    before(function () {
+        cy.visit(Cypress.env('urlProd'))
+        cy.fixture("LMS/TeacherLoginCredentials").then(function (validAdminLoginData) {
+            cy.TeacherPostSetupLogin(validAdminLoginData.user3, validAdminLoginData.password)
+        })
+    })
+
+    beforeEach(function () {
+        cy.viewport(1920, 1080)
+        cy.fixture("LMS/TeacherDashboardCredentials").as("teacherDashboardCredentials")
+    })
+
+    it('EL-6975/ES6975-02,ES6975-03 Validate system is displaying attendance dynamically in the gradebook irrespective of gradebook updates', function () {
+        
+        
+    })
+
+})

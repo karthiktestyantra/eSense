9c487bd6a1ac862dc31a058e5fcf6e0a09c3e5ab
karthiktesyantra <karthik.k@testyantra.com>
2022-10-17 15:07:28 +0530

    commit

--- cypress/e2e/LMS-Prod/Admin/Admin-ContentLibrary.js
+++ cypress/e2e/LMS-Prod/Admin/Admin-ContentLibrary.js
@@ -238,24 +238,4 @@ describe("Verify  Content Library functionalities", function () {
         cy.isVisible(adminContentLibraryPage.getElaAssignmentsTab())
     })
 
-
-    it('EL-768/ES768-01 To validate user is able to upload personal resources personal resources like ( videos, pdf, doc, interactive contents ) can be saved under my personal content library.',function () {
-        cy.visit(Cypress.env('urlProd'))
-        cy.fixture("LMS/ValidTeacherLoginCredentials.json").then(function (validAdminLoginData) {
-            this.validAdminLoginData = validAdminLoginData;
-            cy.TeacherPostSetupLogin(this.validAdminLoginData.prodUsername, this.validAdminLoginData.prodPassword)
-        })
-        adminDashboardPage.getContentLibraryBtn().click({force:true});
-        adminContentLibraryPage.getTopSchoolLibrary().click()
-        myPersonalLibraryPage.getMyPersonalLibraryTab().click()
-        cy.wait(1000)
-        myPersonalLibraryPage.getUploadResource().scrollIntoView().selectFile('cypress/fixtures/LMS/ErrorReport.xlsx',{force:true})
-    })
-    it('EL-768/ES768-04  To validate after successful upload file will contain tags like type of content (radio button) and  class, grade, chapter topic from the drop down.',function () {
-        cy.wait(2000)
-        cy.isVisible(myPersonalLibraryPage.getUploadResoureceGradeDropdown())
-        cy.isVisible(myPersonalLibraryPage.getUploadResoureceSubjectDropdown())
-        cy.isVisible(myPersonalLibraryPage.getUploadResoureceChapterDropdown())
-        cy.isVisible(myPersonalLibraryPage.getUploadResoureceTopicsDropdown())
-    })
 })

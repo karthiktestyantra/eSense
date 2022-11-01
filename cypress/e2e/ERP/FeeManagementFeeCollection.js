51f89c9ffe62de224ebe23de2e6562f8fc0f2aee
harshith <harshith.s@testyantra.com>
2022-11-01 11:11:57 +0530

    coomit

--- cypress/e2e/ERP/FeeManagementFeeCollection.js
+++ cypress/e2e/ERP/FeeManagementFeeCollection.js
@@ -1,6 +1,7 @@
 const loginPage = require('../../support/pageObjects/ERP/LoginPage')
 const adminDashboardPage = require('../../support/pageObjects/ERP/AdminDashboardPage')
 const feeSetUpOnBoardingPage = require('../../support/pageObjects/ERP/FeeSetUpOnBoardingPage')
+const feeManagementFeeCollectionPage = require('../../support/pageObjects/ERP/FeeManagementFeeCollectionPage')
 const dayjs = require('dayjs')
 describe("Verify Fee Collection functionalities", function () {
 
@@ -14,10 +15,30 @@ describe("Verify Fee Collection functionalities", function () {
       })
     
       beforeEach(function () {
-        cy.fixture("ERP/FeeSetUpOnBoarding").as("feeSetUpOnBoarding")
+        cy.fixture("ERP/FeeCollection").as("feeCollection")
       })
 
       it("TC_001 - Validate user is able to collect fee", function () {
+        adminDashboardPage.navigateToFeeSetUpPage()
+        adminDashboardPage.getFeeCollectionLink().click()
+        cy.wait(4000)
+        feeManagementFeeCollectionPage.getStudentSearchBtn().type(this.feeCollection.StudentSearchTxt)
+        cy.wait(3000)
+        feeManagementFeeCollectionPage.getListOfStudentName().each(($e1,index,$list)=>{
+            var studentNameTxt = $e1.text().trim()
+            if(studentNameTxt===(this.feeCollection.StudentSearchTxt)){
+            feeManagementFeeCollectionPage.getListOfStudentCollectFeeBtn().eq(index).click()
+
+            }
+
+        })
+        feeManagementFeeCollectionPage.getPaymentModeDropdown().click()
+        feeManagementFeeCollectionPage.getPaymentModeDropdownList().contains(this.feeCollection.paymentmode).click()
+        feeManagementFeeCollectionPage.getFeeAmtTxtField().type(this.feeCollection.FeeAmtValue)
+        feeManagementFeeCollectionPage.getFeeTransactionIdTxtField().type(this.feeCollection.TransactionID)
+        feeManagementFeeCollectionPage.getFeeAttachFile().attachFile('ERP/sample.pdf');
+        feeManagementFeeCollectionPage.getFeeRemarksTextArea().type(this.feeCollection.Remarks)
+        feeManagementFeeCollectionPage.getFeeCollectionApplyBtn().click()
         
       })
 })  
---
+++ cypress/support/pageObjects/ERP/FeeManagementFeeCollectionPage.js
@@ -0,0 +1,46 @@
+class FeeManagementFeeCollectionPage{
+
+    getListOfStudentName() {
+        return cy.get('p.name')
+    }
+
+    getStudentSearchBtn() {
+        return cy.get('input[placeholder="Search..."]')
+    }
+
+    getListOfStudentCollectFeeBtn() {
+        return cy.get('p.collectFees')
+    }
+
+    getPaymentModeDropdown() {
+        return cy.get(':nth-child(1) > .MuiFormControl-root > .MuiInputBase-root > #demo-simple-select')
+    }
+
+    getPaymentModeDropdownList() {
+        return cy.get('ul.MuiMenu-list li')
+    }
+    
+    getFeeAmtTxtField() {
+        return cy.get('input[name="amount"]')
+    }
+
+    getFeeTransactionIdTxtField() {
+        return cy.get('input[name="transactionId"]')
+    }
+
+    getFeeAttachFile() {
+        return cy.get('input.fileUpload')
+    }
+
+    getFeeRemarksTextArea() {
+        return cy.get('textarea[name="remark"]')
+    }
+
+    getFeeCollectionApplyBtn() {
+        return cy.get('button[data-testid="applyBtn"]')
+    }
+
+
+
+}
+module.exports = new FeeManagementFeeCollectionPage()

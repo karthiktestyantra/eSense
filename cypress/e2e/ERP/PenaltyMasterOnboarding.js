daa17d197f48cdc49e707297cc1641f5c1c1ff2d
harshith <harshith.s@testyantra.com>
2022-11-04 14:18:23 +0530

    commit

--- cypress/e2e/ERP/PenaltyMasterOnboarding.js
+++ cypress/e2e/ERP/PenaltyMasterOnboarding.js
@@ -7,9 +7,9 @@ describe("Verify Penalty Master functionalities", function () {
     before(function () {
         cy.clearCookies();
         cy.clearLocalStorage();
-        cy.visit(Cypress.env("urlPostSetupERP"))
+        cy.visit(Cypress.env("urlPreSetupERP"))
         cy.fixture("ERP/LoginCredentials").then(function (loginCredentials) {
-          loginPage.login(loginCredentials.adminUsernamePostSetUp, loginCredentials.adminPassword)
+          loginPage.login(loginCredentials.adminUsernamePreSetUp, loginCredentials.adminPassword)
         })
         cy.saveLocalStorage();
       })
@@ -28,20 +28,51 @@ describe("Verify Penalty Master functionalities", function () {
       it("ERP_TC_002 - Validate user is navigate from Fee structure master to Penalty master module tab.", function () {
         adminDashboardPage.navigateToFeeSetUpPage()
         penaltyMasterOnBoardingPage.getPresetupPenaltyMasterContinueBtn().click()
+      // cy.get('.MuiTabs-flexContainer > :nth-child(2)').click()
         penaltyMasterOnBoardingPage.getPenaltyMasterTitleTxt().should('have.text',' Penalty Master ')
+        cy.wait(3000)
         penaltyMasterOnBoardingPage.getPenaltyMasterAddBtn().should('be.visible')
 
       })
 
-      it.skip("ERP_TC_003 - Validate user is create the penalty by using the 'Setup Penalty Masters' button", function () {
+      // it.skip("ERP_TC_003 - Validate user is create the penalty by using the 'Setup Penalty Masters' button", function () {
 
-      })
+      // })
 
       it("ERP_TC_004 - Validate user is navigate from Fee structure master to Penalty master module tab.", function () {
+       // penaltyMasterOnBoardingPage.createAndVerifyPenaltyMasterFunctionalities(this.PenaltyMaster.PenaltyTypeValue, this.PenaltyMaster.penaltyName,this.PenaltyMaster.PenaltyAmountValue,this.PenaltyMaster.unpaidAmt,this.PenaltyMaster.chargePerDay,this.PenaltyMaster.durationValue) 
         penaltyMasterOnBoardingPage.getPenaltyMasterAddBtn().click()
         penaltyMasterOnBoardingPage.getPenaltyNameTextField().type(this.PenaltyMaster.penaltyName)
         penaltyMasterOnBoardingPage.getPenaltyTypeDropdown().click()
         penaltyMasterOnBoardingPage.getPenaltyTypeDropdownValue().contains(this.PenaltyMaster.PenaltyTypeValue).click()
+        penaltyMasterOnBoardingPage.getPenaltySelectGradeTypeDropdown().click()
+        penaltyMasterOnBoardingPage.getPenaltySelectAllGradeDropdownValues().each(($e1,index,$list)=>{
+         cy.wrap($e1).uncheck()
+         cy.wrap($e1).check()
+        })
+        cy.clickOnBody()
+        penaltyMasterOnBoardingPage.getPenaltySelectStreamDropdown().click()
+        penaltyMasterOnBoardingPage.getPenaltySelectStreamDropdownValues().each(($e1,index,$list)=>{
+         cy.wrap($e1).uncheck()
+         cy.wrap($e1).check()
+        })
+        cy.clickOnBody()
+        penaltyMasterOnBoardingPage.getPenaltyAmountFlatAmt().click()
+        penaltyMasterOnBoardingPage.getPenaltyAmountTxtField().type(this.PenaltyMaster.PenaltyAmountValue)
+        penaltyMasterOnBoardingPage.getpenaltyUnpaidLink().click()
+        penaltyMasterOnBoardingPage.getpenaltyUnpaidAmtTextField().type(this.PenaltyMaster.unpaidAmt)
+        penaltyMasterOnBoardingPage.getpenaltyChargePer().type(this.PenaltyMaster.chargePerDay) 
+        penaltyMasterOnBoardingPage.getpenaltyDurationDropdown().click()
+        penaltyMasterOnBoardingPage.getpenaltyDurationDropdownValue().contains(this.PenaltyMaster.durationValue).click()
+        penaltyMasterOnBoardingPage.getpenaltyMasterSaveBtn().click()
+        cy.on('window:alert',(t)=>{
+          //assertions
+          expect(t).to.contains('New Penalty Created');
+       })
+
+      })
+
+      it("ERP_TC_004 - Validate user is navigate from Fee structure master to Penalty master module tab.", function () {
 
 
       })
--- cypress/support/pageObjects/ERP/PenaltyMasterOnBoardingPage.js
+++ cypress/support/pageObjects/ERP/PenaltyMasterOnBoardingPage.js
@@ -23,6 +23,93 @@ class PenaltytMasterOnBoardingPage {
     getPenaltyTypeDropdownValue() {
         return cy.get('span.MuiListItemText-primary')
     }
+    
+    getPenaltySelectGradeTypeDropdown() {
+        return cy.get('div.MuiSelect-select').eq(1)
+    }
+      
+    getPenaltySelectAllGradeDropdownValues() {
+        return cy.get('span.MuiCheckbox-root input')
+    }
+
+    getPenaltySelectStreamDropdown() {
+        return cy.get('div.MuiSelect-select').eq(2)
+    }
+     
+    getPenaltySelectStreamDropdownValues() {
+        return cy.get('span.MuiCheckbox-root input')
+    }
+
+    getPenaltyAmountFlatAmt() {
+        return cy.get('input[name="penaltyAmountType"]').eq(0)
+    }
+
+    getPenaltyAmountTxtField() {
+        return cy.get('div.MuiTextField-root').eq(2)
+    }
+
+    getpenaltyUnpaidLink() {
+        return cy.get('div[data-testid="penaltyUnpaid"]')
+    }
+    
+    getpenaltyUnpaidAmtTextField() {
+        return cy.get('input[name="unpaidAmount"]')
+    }
+
+    getpenaltyUnpaidAmtTextField() {
+        return cy.get('input[name="unpaidAmount"]')
+    }
+    
+    getpenaltyChargePer() {
+        return cy.get('input[name="duration"]')
+    }
+
+    getpenaltyDurationDropdown() {
+        return cy.get('div[aria-labelledby="demo-multiple-checkbox-label demo-multiple-checkbox"]').eq(3)
+    }
+
+    getpenaltyDurationDropdownValue() {
+        return cy.get('ul.MuiMenu-list li')
+    }
+    
+    getpenaltyMasterSaveBtn() {
+        return cy.get('button.MuiButtonBase-root').contains('Save')
+    }
+
+    // //Bussiness Logic
+    // createAndVerifyPenaltyMasterFunctionalities(PenaltyTypeValue, penaltyName,PenaltyAmountValue,unpaidAmt,chargePerDay,durationValue) {
+    //     cy.wait(1000)
+    //     this.getPenaltyMasterAddBtn().click()
+    //     this.getPenaltyNameTextField().type(penaltyName)
+    //     this.getPenaltyTypeDropdown().click()
+    //     this.getPenaltyTypeDropdownValue().contains(PenaltyTypeValue).click()
+    //     this.getPenaltySelectGradeTypeDropdown().click()
+    //     this.getPenaltySelectAllGradeDropdownValues().each(($e1,index,$list)=>{
+    //      cy.wrap($e1).uncheck()
+    //      cy.wrap($e1).check()
+    //     })
+    //     cy.clickOnBody()
+    //     this.getPenaltySelectStreamDropdown().click()
+    //     this.getPenaltySelectStreamDropdownValues().each(($e1,index,$list)=>{
+    //      cy.wrap($e1).uncheck()
+    //      cy.wrap($e1).check()
+    //     })
+    //     cy.clickOnBody()
+    //     this.getPenaltyAmountFlatAmt().click()
+    //     this.getPenaltyAmountTxtField().type(PenaltyAmountValue)
+    //     this.getpenaltyUnpaidLink().click()
+    //     this.getpenaltyUnpaidAmtTextField().type(unpaidAmt)
+    //     this.getpenaltyChargePer().type(chargePerDay) 
+    //     this.getpenaltyDurationDropdown().click()
+    //     this.getpenaltyDurationDropdownValue().contains(durationValue).click()
+    //     this.getpenaltyMasterSaveBtn().click()
+    //     cy.on('window:alert',(t)=>{
+    //       //assertions
+    //       expect(t).to.contains('New Penalty Created');
+    //    })
+
+   // }
+
 
 
 

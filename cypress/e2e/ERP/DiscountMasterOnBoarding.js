72076a4095dcbb1be2c7672e1015db05cd8fed91
karthiktestyantra <karthik.k@testyantra.com>
2022-11-14 12:43:41 +0530

    commit

--- cypress/e2e/ERP/DiscountMasterOnBoarding.js
+++ cypress/e2e/ERP/DiscountMasterOnBoarding.js
@@ -4,7 +4,7 @@ const feeSetUpOnBoardingPage = require('../../support/pageObjects/ERP/FeeSetUpOn
 const discountMasterOnBoardingPage = require('../../support/pageObjects/ERP/DiscountMasterOnBoardingPage')
 const dayjs = require('dayjs')
 
-describe("Verify Fee SetUp OnBoarding functionalities", function () {
+describe("Verify Discount Master OnBoarding functionalities", function () {
 
   before(function () {
     cy.clearCookies();
@@ -19,43 +19,244 @@ describe("Verify Fee SetUp OnBoarding functionalities", function () {
   beforeEach(function () {
     cy.restoreLocalStorage();
     cy.fixture("ERP/PenaltyMasterCredentials").as("PenaltyMaster")
+    cy.viewport(1920,1080)
   })
 
+  it("TC_001 - Validate user is able to create a discount master in fresh page", function () {
+    adminDashboardPage.getSideMenuFeeManagementIcon().click()
+    cy.wait(1000)
+    cy.get('body').then(($el) => {
+        if ($el.find('[class="popper-sub-list"]').length > 0) {
+          adminDashboardPage.getFeeSetupLink().click()
+        }
+    })
+    cy.wait(1500)
+    // feeSetUpOnBoardingPage.getFeeStructureContinueBtn().click()
+    // cy.wait(1000)
+    // feeSetUpOnBoardingPage.getFeeStructureContinueBtn().click()
+    // cy.wait(1000)
+    cy.get('div.MuiGrid-container div.MuiGrid-item').eq(3).click()
+    cy.wait(1000)
+    discountMasterOnBoardingPage.getDefaultTxtInDiscountMaster().should('have.text'," No Discount Master Found ")
+    discountMasterOnBoardingPage.getSetupDiscountMasters().should('be.visible')
+    discountMasterOnBoardingPage.getSetupDiscountMasters().click()
+    cy.wait(1000)
+    discountMasterOnBoardingPage.getDiscountNameBtn().click().type("Basic")
+    discountMasterOnBoardingPage.getDiscountTypeDrpDwn().trigger('mouseover').click({force:true})
+    discountMasterOnBoardingPage.getDiscountTypeLstInDiscountTypeDrpDwn().should('contain.text',"Quota").and('contain.text',"Sibling").
+    and('contain.text',"Scholarship").and('contain.text',"Employees Child Discount").and('contain.text',"Children with special needs").
+    and('contain.text',"Economically weaker section").and('contain.text',"Upfront Payment").and('contain.text',"Other")
+    discountMasterOnBoardingPage.getDiscountTypeLstInDiscountTypeDrpDwn().contains("Quota").click()
+    discountMasterOnBoardingPage.getGeneralCheckBxLst().should('have.length',2)
+    discountMasterOnBoardingPage.getDiscountAmountTxtFld().click().type(1000)
+    discountMasterOnBoardingPage.getCheckBxNameLst().should('contain.text',"General").and('contain.text',"Flat Amount").and('contain.text',"Percentage").
+    and('contain.text',"SC").and('contain.text',"ST").and('contain.text',"OBC")
+    discountMasterOnBoardingPage.getSaveBtn().should('be.visible')
+    discountMasterOnBoardingPage.getSaveAndAddNewBtn().should('be.visible')
+    discountMasterOnBoardingPage.getCancelBtn().should('be.visible')
+    discountMasterOnBoardingPage.getSaveBtn().click()
+    cy.contains("New Discount Created").should('be.visible')
+    cy.wait(1000)
+  })
 
   it("TC_002 - Validate user is able to create a discount master", function () {
-    adminDashboardPage.navigateToFeeSetUpPage()
     cy.wait(1000)
-    feeSetUpOnBoardingPage.clickOnFeeStructureContinueBtn()
+    cy.uncaughtException()
+    discountMasterOnBoardingPage.getDiscountMasterAddNewBtn().click()
     cy.wait(1000)
-    feeSetUpOnBoardingPage.clickOnFeeStructureContinueBtn()
-    cy.wait(1000)
-    discountMasterOnBoardingPage.clickOnAddNewBtn()
-    discountMasterOnBoardingPage.fillingTheDetailsInDiscountMasterPopUp()
-    discountMasterOnBoardingPage.verifyDisountMasterFunctionalities()
-    cy.wait(1000)
-    discountMasterOnBoardingPage.verifyCreatedDiscountNameInDiscountMasterPage()
+    discountMasterOnBoardingPage.getDiscountNameBtn().click().type("Basic")
+    discountMasterOnBoardingPage.getDiscountTypeDrpDwn().trigger('mouseover').click({force:true})
+    discountMasterOnBoardingPage.getDiscountTypeLstInDiscountTypeDrpDwn().should('contain.text',"Quota").and('contain.text',"Sibling").
+    and('contain.text',"Scholarship").and('contain.text',"Employees Child Discount").and('contain.text',"Children with special needs").
+    and('contain.text',"Economically weaker section").and('contain.text',"Upfront Payment").and('contain.text',"Other")
+    discountMasterOnBoardingPage.getDiscountTypeLstInDiscountTypeDrpDwn().contains("Quota").click()
+    discountMasterOnBoardingPage.getGeneralCheckBxLst().should('have.length',2)
+    discountMasterOnBoardingPage.getDiscountAmountTxtFld().click().type(1000)
+    discountMasterOnBoardingPage.getCheckBxNameLst().should('contain.text',"General").and('contain.text',"Flat Amount").and('contain.text',"Percentage").
+    and('contain.text',"SC").and('contain.text',"ST").and('contain.text',"OBC")
+    discountMasterOnBoardingPage.getSaveBtn().should('be.visible')
+    discountMasterOnBoardingPage.getSaveAndAddNewBtn().should('be.visible')
+    discountMasterOnBoardingPage.getCancelBtn().should('be.visible')
+    discountMasterOnBoardingPage.getSaveBtn().click()
+    cy.contains("New Discount Created").should('be.visible')
+    cy.wait(3000)
+    discountMasterOnBoardingPage.getDiscountNameLst().each(($e1,index,$list)=>{
+      const text = $e1.text()
+      if(text.includes("Basic")){
+        discountMasterOnBoardingPage.getDiscountTypeLst().eq(index).should('have.text',"Quota")
+        discountMasterOnBoardingPage.getSubTypeLst().eq(index).should('have.text',"General")
+        discountMasterOnBoardingPage.getEditIconsLst().eq(index).should('be.visible')
+        discountMasterOnBoardingPage.getDeleteIconsLst().eq(index).should('be.visible')
+      }
+  })
   })
 
   it("TC_003 - Validate user is able to create a discount master after click on Save and Add Button", function () {
     cy.wait(3000)
-    discountMasterOnBoardingPage.clickOnAddNewBtn()
+    discountMasterOnBoardingPage.getDiscountMasterAddNewBtn().click()
     cy.wait(1000)
-    discountMasterOnBoardingPage.fillingTheDetailsInDiscountMasterPopUp()
+    discountMasterOnBoardingPage.getDiscountNameBtn().click().type("Basic")
+    discountMasterOnBoardingPage.getDiscountTypeDrpDwn().trigger('mouseover').click({force:true})
+    discountMasterOnBoardingPage.getDiscountTypeLstInDiscountTypeDrpDwn().should('contain.text',"Quota").and('contain.text',"Sibling").
+    and('contain.text',"Scholarship").and('contain.text',"Employees Child Discount").and('contain.text',"Children with special needs").
+    and('contain.text',"Economically weaker section").and('contain.text',"Upfront Payment").and('contain.text',"Other")
+    discountMasterOnBoardingPage.getDiscountTypeLstInDiscountTypeDrpDwn().contains("Quota").click()
+    discountMasterOnBoardingPage.getGeneralCheckBxLst().should('have.length',2)
+    discountMasterOnBoardingPage.getDiscountAmountTxtFld().click().type(1000)
+    discountMasterOnBoardingPage.getCheckBxNameLst().should('contain.text',"General").and('contain.text',"Flat Amount").and('contain.text',"Percentage").
+    and('contain.text',"SC").and('contain.text',"ST").and('contain.text',"OBC")
+    discountMasterOnBoardingPage.getSaveBtn().should('be.visible')
+    discountMasterOnBoardingPage.getSaveAndAddNewBtn().should('be.visible')
+    discountMasterOnBoardingPage.getCancelBtn().should('be.visible')
     discountMasterOnBoardingPage.verifyDisountMasterFunctionalitiesWithSaveAndAdd()
   })
 
   it("TC_004 - Validate user is able to cancel a new  discount master", function () {
     cy.wait(1000)
-    discountMasterOnBoardingPage.clickOnAddNewBtn()
-    discountMasterOnBoardingPage.fillingTheDetailsInDiscountMasterPopUp()
-    discountMasterOnBoardingPage.verifyDisountMasterFunctionalitiesWithCancel()
+    discountMasterOnBoardingPage.getDiscountMasterAddNewBtn().click()
+    cy.wait(1000)
+    discountMasterOnBoardingPage.getDiscountNameBtn().click().type("Basic")
+    discountMasterOnBoardingPage.getDiscountTypeDrpDwn().trigger('mouseover').click({force:true})
+    discountMasterOnBoardingPage.getDiscountTypeLstInDiscountTypeDrpDwn().should('contain.text',"Quota").and('contain.text',"Sibling").
+    and('contain.text',"Scholarship").and('contain.text',"Employees Child Discount").and('contain.text',"Children with special needs").
+    and('contain.text',"Economically weaker section").and('contain.text',"Upfront Payment").and('contain.text',"Other")
+    discountMasterOnBoardingPage.getDiscountTypeLstInDiscountTypeDrpDwn().contains("Quota").click()
+    discountMasterOnBoardingPage.getGeneralCheckBxLst().should('have.length',2)
+    discountMasterOnBoardingPage.getDiscountAmountTxtFld().click().type(1000)
+    discountMasterOnBoardingPage.getCheckBxNameLst().should('contain.text',"General").and('contain.text',"Flat Amount").and('contain.text',"Percentage").
+    and('contain.text',"SC").and('contain.text',"ST").and('contain.text',"OBC")
+    discountMasterOnBoardingPage.getSaveBtn().should('be.visible')
+    discountMasterOnBoardingPage.getSaveAndAddNewBtn().should('be.visible')
+    discountMasterOnBoardingPage.getCancelBtn().should('be.visible')
+    discountMasterOnBoardingPage.getCancelBtn().click()
+    discountMasterOnBoardingPage.getAddDiscountPopup().should('not.exist')
+  })
+
+  it("TC_008 Validate user is able to delete Discount master", function () {
+    cy.wait(1000)
+    discountMasterOnBoardingPage.getDiscountNameLst().each(($e1,index,$list)=> {
+      const txt = $e1.text()
+      if(txt === "Basic"){
+        discountMasterOnBoardingPage.getDicountNameDeleteIconLst().eq(0).click()
+          cy.contains("Delete Basic ?").should('be.visible')
+          discountMasterOnBoardingPage.getDiscountNameDltCnfirmBtn().click()
+          cy.contains("Basic has been deleted from discount master.").should('be.visible')
+          discountMasterOnBoardingPage.getCloseDiscountDeletedPopup().click()
+          cy.wait(1000)
+      }
+  })
   })
 
   it("TC_005 Validate user is able to create a Quota discount with custom quota", function () {
     cy.wait(1000)
-    discountMasterOnBoardingPage.clickOnAddNewBtn()
-    discountMasterOnBoardingPage.fillingTheDetailsInDiscountMasterPopUp()
-    discountMasterOnBoardingPage.verifyQuotaDiscountFunctionalities()
+    discountMasterOnBoardingPage.getDefaultTxtInDiscountMaster().should('have.text'," No Discount Master Found ")
+    discountMasterOnBoardingPage.getSetupDiscountMasters().should('be.visible')
+    discountMasterOnBoardingPage.getSetupDiscountMasters().click()
+    cy.wait(1000)
+    discountMasterOnBoardingPage.getDiscountNameBtn().click().type("Basic")
+    discountMasterOnBoardingPage.getDiscountTypeDrpDwn().trigger('mouseover').click({force:true})
+    discountMasterOnBoardingPage.getDiscountTypeLstInDiscountTypeDrpDwn().should('contain.text',"Quota").and('contain.text',"Sibling").
+    and('contain.text',"Scholarship").and('contain.text',"Employees Child Discount").and('contain.text',"Children with special needs").
+    and('contain.text',"Economically weaker section").and('contain.text',"Upfront Payment").and('contain.text',"Other")
+    discountMasterOnBoardingPage.getDiscountTypeLstInDiscountTypeDrpDwn().contains("Quota").click()
+    discountMasterOnBoardingPage.getGeneralCheckBxLst().should('have.length',2)
+    discountMasterOnBoardingPage.getDiscountAmountTxtFld().click().type(1000)
+    discountMasterOnBoardingPage.getCheckBxNameLst().should('contain.text',"General").and('contain.text',"Flat Amount").and('contain.text',"Percentage").
+    and('contain.text',"SC").and('contain.text',"ST").and('contain.text',"OBC")
+    discountMasterOnBoardingPage.getSaveBtn().should('be.visible')
+    discountMasterOnBoardingPage.getSaveAndAddNewBtn().should('be.visible')
+    discountMasterOnBoardingPage.getCancelBtn().should('be.visible')
+    discountMasterOnBoardingPage.getAddQuotaBtn().click()
+    discountMasterOnBoardingPage.getQuotaNameFld().type("Sports")
+    discountMasterOnBoardingPage.getDiscountAmountTxtFldFrCustom().type("3000")
+    discountMasterOnBoardingPage.getSaveBtn().click()
+    cy.contains("New Discount Created").should('be.visible')
+    discountMasterOnBoardingPage.getDiscountNameLst().each(($e1,index,$list)=> {
+        const txt = $e1.text()
+        if(txt === "Basic"){
+          discountMasterOnBoardingPage.getCustomQuotaNotifyIconLst().eq(0).find('span').should('have.text',"+1")
+        }
+    })
+  })
+
+  it("TC_007 Validate user is able to edit discount master",function () {
+    cy.wait(1000)
+    discountMasterOnBoardingPage.getDiscountNameLst().each(($e1,index,$list)=> {
+      const txt = $e1.text()
+      if(txt === "Basic"){
+        discountMasterOnBoardingPage.getDicountNameEditIconLst().eq(0).click()
+        return false;
+      }
+    })
+    cy.wait(1000)
+    discountMasterOnBoardingPage.getDiscountNameBtn().click().clear()
+    cy.wait(1000)
+    discountMasterOnBoardingPage.getDiscountNameBtn().click().type("love")
+    cy.wait(1000)
+    discountMasterOnBoardingPage.getSaveBtn().click()
+    cy.contains("Discount details updated successfully").should('be.visible')
+    cy.wait(1000)
+    discountMasterOnBoardingPage.getDiscountNameLst().should('contain.text',"love")
+  })
+
+  it("TC_006 Validate user is able to create a discount master with scholorship",function () {
+    cy.wait(1000)
+    discountMasterOnBoardingPage.getDiscountMasterAddNewBtn().click()
+    cy.wait(1000)
+    discountMasterOnBoardingPage.getDiscountNameBtn().click().type("Basic")
+    discountMasterOnBoardingPage.getDiscountTypeDrpDwn().trigger('mouseover').click({force:true})
+    discountMasterOnBoardingPage.getDiscountTypeLstInDiscountTypeDrpDwn().contains("Scholarship").click()
+    discountMasterOnBoardingPage.getNewStudentTabInScholarship().should('be.visible').and('be.enabled')
+    discountMasterOnBoardingPage.getExistingStudentTabInScholarship().should('be.visible').and('be.enabled')
+    discountMasterOnBoardingPage.getDuplicateFrmExistingStudentsCheckBx().check()
+    discountMasterOnBoardingPage.getExistingStudentTabInScholarship().click()
+    discountMasterOnBoardingPage.getDuplicateFrmNewStudentsCheckBx().check()
+    discountMasterOnBoardingPage.getNewStudentTabInScholarship().click()
+    discountMasterOnBoardingPage.getAddMarksRange().should('be.visible').click()
+    //discountMasterOnBoardingPage.getMarksRangeMinSlider().should('be.visible')
+    cy.wait(1000)
+    discountMasterOnBoardingPage.getAddMarksRange().should('be.visible').click()
+    discountMasterOnBoardingPage.getScholarshipAmountTxtFld1().type('500')
+    discountMasterOnBoardingPage.getScholarshipAmountTxtFld2().type('500')
+    discountMasterOnBoardingPage.getSaveBtn().click()
+    cy.contains("Marks range should not overlap.").should('be.visible')
+    discountMasterOnBoardingPage.getDeleteMarksRangeDeleteBtnLst().last().click()
+    discountMasterOnBoardingPage.getSaveBtn().click()
+    cy.contains("New Discount Created").should('be.visible')
+    cy.wait(3000)
+    discountMasterOnBoardingPage.getDiscountNameLst().each(($e1,index,$list)=>{
+      const text = $e1.text()
+      if(text.includes("Basic")){
+        discountMasterOnBoardingPage.getDiscountTypeLst().eq(index).should('have.text',"Scholarship")
+        discountMasterOnBoardingPage.getSubTypeLst().eq(index).should('have.text',"New")
+      }
+    })
+  })
+
+  it("TC_009 Validate user is able to sort the record by discount name",function () {
+    discountMasterOnBoardingPage.getDiscountNameSortIcn().click()
+    discountMasterOnBoardingPage.getDiscountNameLst().eq(0).should('have.text',"love")
+  })
+
+  it("TC_010 Validate user is able to sort the record by discount type",function () {
+    discountMasterOnBoardingPage.getDiscountTypeSortIcn().click()
+    discountMasterOnBoardingPage.getDiscountNameLst().eq(0).should('have.text',"Basic")
+  })
+
+  //Post-condition
+
+  it("To Delete all the discounts", function () {
+    cy.wait(1000)
+    discountMasterOnBoardingPage.getDiscountNameLst().each(($e1,index,$list)=> {
+      if(index<$list.length){
+        discountMasterOnBoardingPage.getDicountNameDeleteIconLst().eq(0).click()
+          cy.contains("Delete").should('be.visible')
+          discountMasterOnBoardingPage.getDiscountNameDltCnfirmBtn().click()
+          cy.contains("has been deleted from discount master.").should('be.visible')
+          discountMasterOnBoardingPage.getCloseDiscountDeletedPopup().click()
+          cy.wait(1000)
+      }
+    })
   })
 
 })
--- cypress/support/pageObjects/ERP/DiscountMasterOnBoardingPage.js
+++ cypress/support/pageObjects/ERP/DiscountMasterOnBoardingPage.js
@@ -9,60 +9,123 @@ class DiscountMasterOnBoardingPage {
     getDiscountTypeDrpDwn() {
         return cy.get('#demo-multiple-discount')
     }
-    getDiscountTypeLstInDiscountTypeDrpDwn(){
+    getDiscountTypeLstInDiscountTypeDrpDwn() {
         return cy.get('div.MuiPaper-elevation ul li div span')
     }
-    getGeneralCheckBxLst(){
+    getGeneralCheckBxLst() {
         return cy.get('div[role="radiogroup"] span.MuiRadio-root')
     }
-    getDiscountAmountTxtFld(){
+    getDiscountAmountTxtFld() {
         return cy.get('input[name="quota.0.discountAmount"]')
     }
-    getDiscountAmountTxtFldFrCustom(){
+    getDiscountAmountTxtFldFrCustom() {
         return cy.get('input[name="quota.1.discountAmount"]')
     }
-    getCheckBxNameLst(){
+    getScholarshipAmountTxtFld1(){
+        return cy.get('input[name="newScholarship.0.discountAmount"]')
+    }
+    getScholarshipAmountTxtFld2(){
+        return cy.get('input[name="newScholarship.1.discountAmount"]')
+    }
+    getCheckBxNameLst() {
         return cy.get('span.MuiFormControlLabel-label')
     }
-    getSaveBtn(){
+    getSaveBtn() {
         return cy.get('button[type="button"]').contains("Save")
     }
-    getSaveAndAddNewBtn(){
+    getSaveAndAddNewBtn() {
         return cy.get('button[type="button"]').contains("Save & Add New")
     }
-    getCancelBtn(){
+    getCancelBtn() {
         return cy.get('button[type="button"]').contains("Cancel")
     }
-    getDiscountNameLst(){
+    getDiscountNameLst() {
         return cy.get('table tbody tr td:nth-child(1)')
     }
-    getDiscountTypeLst(){
+    getDiscountTypeLst() {
         return cy.get('table tbody tr td:nth-child(2)')
     }
-    getSubTypeLst(){
+    getSubTypeLst() {
         return cy.get('table tbody tr td:nth-child(3)')
     }
-    getEditIconsLst(){
+    getEditIconsLst() {
         return cy.get('div img[class="editIcon"]')
     }
-    getDeleteIconsLst(){
+    getDeleteIconsLst() {
         return cy.get('div img[class="deleteIcon"]')
     }
-    getAddDiscountPopup(){
+    getAddDiscountPopup() {
         return cy.get('div[role="dialog"]')
     }
-    getCloseBtnFrAddDiscountPopup(){
+    getCloseBtnFrAddDiscountPopup() {
         return cy.get('#customized-dialog-title > .MuiButtonBase-root > [data-testid="CloseIcon"]')
     }
-    getAddQuotaBtn(){
+    getAddQuotaBtn() {
         return cy.get('button[data-testid="addQuotaText"]')
     }
-    getQuotaNameFld(){
+    getQuotaNameFld() {
         return cy.get('input[name="quota.1.customQuotaName"]')
+    }
+    getDefaultTxtInDiscountMaster() {
+        return cy.get('div.innerBox p')
+    }
+    getSetupDiscountMasters() {
+        return cy.get('button.customBtn').contains("Setup Discount Masters")
+    }
+    getDiscountNameLst() {
+        return cy.get('table tbody tr td.tableFirstCell')
+    }
+    getDicountNameDeleteIconLst() {
+        return cy.get('div.deleteBox img[class="deleteIcon"]')
+    }
+    getDicountNameEditIconLst() {
+        return cy.get('div.deleteBox img[class="editIcon"]')
+    }
+    getDiscountNameDltCnfirmBtn() {
+        return cy.get('button[type="submit"]:visible').contains("Delete")
+    }
+    getCloseDiscountDeletedPopup() {
+        return cy.get('button svg[data-testid="CloseIcon"]:visible')
+    }
+    getCustomQuotaNotifyIconLst() {
+        return cy.get('table tbody tr td.tableSecondCell div')
+    }
+    getNewStudentTabInScholarship() {
+        return cy.get('button[role="tab"]').contains("New Student")
+    }
+    getExistingStudentTabInScholarship() {
+        return cy.get('button[role="tab"]').contains("Existing Student")
+    }
+    getDuplicateFrmExistingStudentsCheckBx() {
+        return cy.get('input[name="isDupliacteFromExisting"]')
+    }
+    getDuplicateFrmNewStudentsCheckBx() {
+        return cy.get('input[name="isDuplicateFromNew"]')
+    }
+    getAddMarksRange() {
+        return cy.get('button[data-testid="addMarksRangeText"]')
+    }
+    getMarksRangeMinSlider() {
+        return cy.get('span.MuiSlider-thumb input[aria-label="Minimum price"]')
+    }
+    getDeleteMarksRangeDeleteBtnLst() {
+        return cy.get('div.deleteBox:visible')
+    }
+    getDiscountNameSortIcn(){
+        return cy.get('div.arrowAlignment img[data-testid="descOrder"]').eq(0)
+    }
+    getDiscountTypeSortIcn(){
+        return cy.get('div.arrowAlignment img[data-testid="descOrder"]').eq(1)
     }
 
     //Business Logic
-    clickOnAddNewBtn(){
+    verifyAndClickOnSetupDiscountMasters() {
+        this.getDefaultTxtInDiscountMaster().should('have.text', " No Discount Master Found ")
+        this.getSetupDiscountMasters().should('be.visible')
+        this.getSetupDiscountMasters().click()
+    }
+
+    clickOnAddNewBtn() {
         this.getDiscountMasterAddNewBtn().click()
     }
 
@@ -81,46 +144,66 @@ class DiscountMasterOnBoardingPage {
         this.getCloseBtnFrAddDiscountPopup().click()
     }
 
-    verifyCreatedDiscountNameInDiscountMasterPage(){
-        this.getDiscountNameLst().each(($e1,index,$list)=>{
+    verifyCreatedDiscountNameInDiscountMasterPage() {
+        this.getDiscountNameLst().each(($e1, index, $list) => {
             const text = $e1.text()
-            if(text.includes("Basic")){
-                this.getDiscountTypeLst().eq(index).should('have.text',"Quota")
-                this.getSubTypeLst().eq(index).should('have.text',"General")
+            if (text.includes("Basic")) {
+                this.getDiscountTypeLst().eq(index).should('have.text', "Quota")
+                this.getSubTypeLst().eq(index).should('have.text', "General")
                 this.getEditIconsLst().eq(index).should('be.visible')
                 this.getDeleteIconsLst().eq(index).should('be.visible')
             }
         })
     }
 
-    fillingTheDetailsInDiscountMasterPopUp(){
+    fillingTheDetailsInDiscountMasterPopUp() {
         cy.wait(1000)
         this.getDiscountNameBtn().click().type("Basic")
-        this.getDiscountTypeDrpDwn().click({force:true})
-        this.getDiscountTypeLstInDiscountTypeDrpDwn().should('contain.text',"Quota").and('contain.text',"Sibling").
-        and('contain.text',"Scholarship").and('contain.text',"Employees Child Discount").and('contain.text',"Children with special needs").
-        and('contain.text',"Economically weaker section").and('contain.text',"Upfront Payment").and('contain.text',"Other")
+        this.getDiscountTypeDrpDwn().trigger('mouseover').click({ force: true })
+        this.getDiscountTypeLstInDiscountTypeDrpDwn().should('contain.text', "Quota").and('contain.text', "Sibling").
+            and('contain.text', "Scholarship").and('contain.text', "Employees Child Discount").and('contain.text', "Children with special needs").
+            and('contain.text', "Economically weaker section").and('contain.text', "Upfront Payment").and('contain.text', "Other")
         this.getDiscountTypeLstInDiscountTypeDrpDwn().contains("Quota").click()
-        this.getGeneralCheckBxLst().should('have.length',2)
+        this.getGeneralCheckBxLst().should('have.length', 2)
         this.getDiscountAmountTxtFld().click().type(1000)
-        this.getCheckBxNameLst().should('contain.text',"General").and('contain.text',"Flat Amount").and('contain.text',"Percentage").
-        and('contain.text',"SC").and('contain.text',"ST").and('contain.text',"OBC")
+        this.getCheckBxNameLst().should('contain.text', "General").and('contain.text', "Flat Amount").and('contain.text', "Percentage").
+            and('contain.text', "SC").and('contain.text', "ST").and('contain.text', "OBC")
         this.getSaveBtn().should('be.visible')
         this.getSaveAndAddNewBtn().should('be.visible')
         this.getCancelBtn().should('be.visible')
     }
 
-    verifyDisountMasterFunctionalitiesWithCancel(){
+    verifyDisountMasterFunctionalitiesWithCancel() {
         this.getCancelBtn().click()
         this.getAddDiscountPopup().should('not.exist')
     }
 
-    verifyQuotaDiscountFunctionalities(){
+    verifyQuotaDiscountFunctionalities() {
         this.getAddQuotaBtn().click()
         this.getQuotaNameFld().type("Sports")
         this.getDiscountAmountTxtFldFrCustom().type("3000")
         this.getSaveBtn().click()
         cy.contains("New Discount Created").should('be.visible')
+        this.getDiscountNameLst().each(($e1, index, $list) => {
+            const txt = $e1.text()
+            if (txt === "Basic") {
+                this.getCustomQuotaNotifyIconLst().eq(0).find('span').should('have.text', "+1")
+            }
+        })
+    }
+
+    verifyAndDeleteTheCreatedDiscountList() {
+        this.getDiscountNameLst().each(($e1, index, $list) => {
+            const txt = $e1.text()
+            if (txt === "Basic" && index < $list.length) {
+                this.getDicountNameDeleteIconLst().eq(0).click()
+                cy.contains("Delete Basic ?").should('be.visible')
+                this.getDiscountNameDltCnfirmBtn().click()
+                cy.contains("Basic has been deleted from discount master.").should('be.visible')
+                this.getCloseDiscountDeletedPopup().click()
+                cy.wait(1000)
+            }
+        })
     }
 
 
